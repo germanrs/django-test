7621f820ffc705c8632a45e701d1b752e5a7e4a8
German Ruiz <gars1029@gmail.com>
2017-05-07 22:50:09 -0400

    Vistas basadas en clases

--- laptop/urls.py
+++ laptop/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, include
 
-from laptop.views import index, laptop_view, laptop_list, laptop_edit, laptop_delete
+from laptop.views import index, laptop_view, laptop_list, laptop_edit, laptop_delete, \
+     LaptopList, LaptopCreate, LaptopUpdate, LaptopDelete
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^nuevo$', laptop_view, name='laptop_crear'),
-    url(r'^listar$', laptop_list, name='laptop_listar'),
-    url(r'^editar/(?P<laptop_id>\d+)/$', laptop_edit, name='laptop_editar'),
-    url(r'^eliminar/(?P<laptop_id>\d+)/$', laptop_delete, name='laptop_eliminar'),
+    url(r'^nuevo$', LaptopCreate.as_view(), name='laptop_crear'),
+    url(r'^listar$', LaptopList.as_view(), name='laptop_listar'),
+    url(r'^editar/(?P<pk>\d+)/$', LaptopUpdate.as_view(), name='laptop_editar'),
+    url(r'^eliminar/(?P<pk>\d+)/$', LaptopDelete.as_view(), name='laptop_eliminar'),
 ]
 
 
--- laptop/views.py
+++ laptop/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from laptop.forms import LaptopForm
 from laptop.models import Laptop
@@ -45,3 +47,24 @@ def laptop_delete(request, laptop_id):
 		laptop.delete()
 		return redirect('laptop:laptop_listar')
 	return render(request, 'laptop/laptop_borrar.html', {'laptop':laptop})
+
+class LaptopList(ListView):
+	model = Laptop
+	template_name = 'laptop/laptop_listar.html'
+
+class LaptopCreate(CreateView):
+	model = Laptop
+	form_class = LaptopForm
+	template_name = 'laptop/laptop_formulario.html'
+	success_url = reverse_lazy('laptop:laptop_listar')
+
+class LaptopUpdate(UpdateView):
+	model = Laptop
+	form_class = LaptopForm
+	template_name = 'laptop/laptop_formulario.html'
+	success_url = reverse_lazy('laptop:laptop_listar')
+
+class LaptopDelete(DeleteView):
+	model = Laptop
+	template_name = 'laptop/laptop_borrar.html'
+	success_url = reverse_lazy('laptop:laptop_listar')
